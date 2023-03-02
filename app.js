e3637101dd55d74b12da7683914f01492051b55c
Lup-lup <maydecember1234@gmail.com>
2023-03-03 02:38:58 +1100

    completed website

---
+++ app.js
@@ -0,0 +1,36 @@
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const slider = document.querySelector(".slider");
+const imgs = document.querySelectorAll(".slider img");
+
+let index = 0;
+let width = imgs[index].clientWidth;
+
+next.addEventListener("click", () => {
+    index++;
+
+    slider.style.transform = `translate(${-index * width}px)`;
+
+    if (index === imgs.length - 1) {
+        next.classList.add("disable");
+    } else{
+        prev.classList.remove("disable");
+    }
+    console.log(index);
+    console.log(imgs.length);
+
+});
+
+prev.addEventListener("click", () => {
+    index--;
+
+    slider.style.transform = `translate(${-index * width}px)`;
+
+    if (index === 0) {
+        prev.classList.add("disable");
+    } else {
+        next.classList.remove("disable");
+    }
+    console.log(index);
+});
+
